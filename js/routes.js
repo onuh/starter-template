e025eb7079adaa1445bc39542b6837b19d99674b
Onuh victor <victor.onuh@st.futminna.edu.ng>
2022-10-17 09:19:52 +0100

    Add files via upload

---
+++ js/routes.js
@@ -0,0 +1,29 @@
+"use strict";
+var routes = [
+  {
+    path: '/',
+    Url: './index.html',
+  },
+
+  {
+    path: '/page1/',
+    url: './pages/page1.html',
+  },
+
+  {
+    path: '/page2/',
+     url: './pages/page2.html',
+  },
+
+  {
+    path: '/page3/',
+     url: './pages/page3.html',
+  },
+
+  {
+    path: '/page4/',
+     url: './pages/page4.html',
+  },
+
+  
+];
